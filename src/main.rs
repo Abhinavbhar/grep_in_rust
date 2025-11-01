0f818641f518d96c0da8cf49a3bf9622d8f864c5
Abhinav bhar <abhinavbhar2004@gmail.com>
2025-11-01 23:38:24 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::{env, error, fs::File, io::Read};
+fn main() {
+    let args:Vec<String>=env::args().collect();
+    let file_path :String = args[1].clone();
+    let word :String = args[2].clone();
+
+    println!("filepath:{},word:{}",file_path,word);
+    let mut file =match File::open(file_path){
+        Ok(file) => file,
+        Err(error)=>{
+            println!("Failed to open the file {}",error);
+            return;
+        }
+    };
+    
+    let mut contents =String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let ans:bool=contents.contains(&word);
+    match ans {
+        true=>println!("found the word"),
+        false=>println!("did not find the word")
+    }
+    //println!("contents of the file is {}",contents);
+
+}
